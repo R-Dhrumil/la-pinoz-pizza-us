63d99f51f6ea0e198c8b68a8a6179301df1e73ff
R-Dhrumil <dhrumilrana516@gmail.com>
2026-02-19 14:11:21 +0530

    feat: Implement Axios API client with request and response interceptors for authentication and error handling.

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'https://api.lapinozusa.com/api';
+// const BASE_URL = 'https://api.lapinozusa.com/api';
+const BASE_URL = 'https://localhost:7100/api';
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
